837b0e27bb8f797e3b0f744de2624e74e2824da6
glebyshkin <glebskobla@tut.by>
2021-10-31 22:37:07 +0300

    complete

---
+++ kata18.js
@@ -0,0 +1,4 @@
+function arrayPlusArray(arr1, arr2, arr3) {
+    let arrayPlusArray = (x,y) => x.concat(y).reduce((a,b) => a + b);
+     return arr3 = arr1.concat(arr2).reduce((x,y)=>x+y); //something went wrong
+   }
