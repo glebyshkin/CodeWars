dbb4144dfc581fba7bb81bd6cd78667c210bbeb4
glebyshkin <glebskobla@tut.by>
2021-11-04 14:13:50 +0300

    complete

---
+++ kata54.js
@@ -0,0 +1,8 @@
+const areaOrPerimeter = function(l , w) {
+    if (l === w) {
+      return l * w;
+    } else if (l > w) {
+      return (l+w) * 2;
+    } else (l < w)
+      return (l + w) * 2;// Return your answer
+  };
